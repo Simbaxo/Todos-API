4e1d94dc35f5ecc6764ea9021fc09de384bfcf69
Andres <ballerlikemike23@gmail.com>
2018-06-24 17:39:46 -0400

    Defining the Create Route

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 let express = require('express'),
     app = express(),
-    port = 3000
+    port = 3000,
+    bodyParser = require('body-parser')
 
 let todoRoutes = require('./routes/todos')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', function(req, res) {
   res.send('Hi There from Express!!!')
