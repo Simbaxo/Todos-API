ac6e37e7e794636a193b3ee84c1216c9927b2467
Andres <ballerlikemike23@gmail.com>
2018-06-24 11:09:38 -0400

    Defining the Index Route

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ let express = require('express'),
     app = express(),
     port = 3000
 
+let todoRoutes = require('./routes/todos')
+
 app.get('/', function(req, res) {
   res.send('Hi There from Express!!!')
 })
+
+app.use('/api/todos', todoRoutes)
 
 app.listen(port, function() {
   console.log('App is running on port 3000...')
