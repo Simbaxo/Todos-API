c1ee7024423c82027ec24c5c004d75c318b22a63
Andres <ballerlikemike23@gmail.com>
2018-06-24 21:42:47 -0400

    Toggling Todo Completion

--- public/app.js
+++ public/app.js
@@ -8,7 +8,12 @@ $(document).ready(function() {
     }
   })
 
-  $('.list').on('click', 'span', function() {
+  $('.list').on('click', 'li', function() {
+    updateTodo($(this))
+  })
+
+  $('.list').on('click', 'span', function(e) {
+    e.stopPropagation()
     removeTodo($(this).parent())
   })
 })
@@ -23,6 +28,7 @@ function addTodos(todos) {
 function addTodo(todo) {
   let newTodo = $('<li class="task">' + todo.name + '<span>X</span></li>')
   newTodo.data('id', todo._id)
+  newTodo.data('completed', todo.completed)
   if(todo.completed) {
     newTodo.addClass('done')
   }
@@ -54,5 +60,20 @@ function removeTodo(todo) {
   })
   .catch(function(err) {
     console.log(err)
+  })
+}
+
+function updateTodo(todo) {
+  let updateUrl = 'api/todos/' + todo.data('id')
+  let isDone = !todo.data('completed')
+  let updateData = {completed: isDone}
+  $.ajax({
+    method: 'PUT',
+    url: updateUrl,
+    data: updateData
+  })
+  .then(function(updatedTodo) {
+    todo.toggleClass('done')
+    todo.data('completed', isDone)
   })
 }
