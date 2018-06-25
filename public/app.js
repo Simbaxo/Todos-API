203a1b9fd4b53fb6e46e068bad3a40fd22c7292d
Andres <ballerlikemike23@gmail.com>
2018-06-24 20:47:26 -0400

    Initial AJAX Call

--- public/app.js
+++ public/app.js
@@ -1,1 +1,12 @@
-alert('Hi from appJS')
+$(document).ready(function() {
+  $.getJSON('/api/todos')
+  .then(addTodos)
+})
+
+function addTodos(todos) {
+  // add todos to page here
+  todos.forEach(function(todo) {
+    let newTodo = $('<li class="task">' + todo.name + '</li>')
+    $('.list').append(newTodo)
+  })
+}
